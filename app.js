dfb43688966f424e599336d9b77c6468c6c75bab
Jef-ux <aalexisbears@gmail.com>
2022-07-04 01:00:24 +0100

    sweet alert

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ function toogleButton() {
 }
 
 hamburgerButton.addEventListener('click', toogleButton)
+
+
+function submitForm(form){
+    swal({
+        title: "Are you sure?",
+        text: "This form will be submitted",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+    .then((isOkay) => {
+        if (isOkay) {
+            swal("Great! Your Application has been succesfully submitted!", {
+                icon: "success",
+              });
+            form.submit();
+        }
+    });
+
+    return false;
+}
